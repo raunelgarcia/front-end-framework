3092e4a1c5ff8e22a75e8f1240d241cbf3fb5901
Alejandra230701 <aalvarti@emeal.nttdata.com>
2024-04-08 12:05:23 +0200

    Reformat code

--- src/test/java/utilities/JSExecutor.java
+++ src/test/java/utilities/JSExecutor.java
@@ -6,24 +6,24 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class JSExecutor {
-    private final WebDriver driver;
-    public JSExecutor(WebDriver driver) {
-        this.driver = driver;
+  private final WebDriver driver;
+
+  public JSExecutor(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public void executeScript(String script, Object... args) {
+    try {
+      JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+      Object result = jsExecutor.executeScript(script, args);
+
+      Allure.step("Script JavaScript ejecutado correctamente", Status.PASSED);
+      System.out.println("Resultado del script JavaScript: " + result);
+
+    } catch (Exception e) {
+
+      Allure.step("Error al ejecutar el script JavaScript: " + e.getMessage(), Status.FAILED);
+      e.printStackTrace();
     }
-    public void executeScript(String script, Object... args) {
-        try {
-            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-            Object result = jsExecutor.executeScript(script, args);
-
-            Allure.step("Script JavaScript ejecutado correctamente", Status.PASSED);
-            System.out.println("Resultado del script JavaScript: " + result);
-
-        } catch (Exception e) {
-
-            Allure.step("Error al ejecutar el script JavaScript: " + e.getMessage(), Status.FAILED);
-            e.printStackTrace();
-        }
-}
-
-
+  }
 }
