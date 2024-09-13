a7fd50b941b1c06703fb141ccf29137acfe09492
Álvaro Laguna García <alvaro.lagunagarcia@emeal.nttdata.com>
2024-09-13 08:50:24 +0200

    Deleted androidConfiguration.yaml and iOSConfiguration.yaml

--- src/main/java/utilities/Constants.java
+++ src/main/java/utilities/Constants.java
@@ -26,8 +26,6 @@ public class Constants {
   public static final String HOTMAIL_DOMAIN = "@hotmail.com";
   public static final String LANGUAGE_REGEX = "[a-zA-Z]{2}-[a-zA-Z]{2}";
   public static final String RESOURCE_PATH = "src/test/resources/";
-  public static final String ANDROID_CONFIG = "yaml/androidConfiguration.yaml";
-  public static final String IOS_CONFIG = "yaml/iOSConfiguration.yaml";
   public static final String WEB_CONFIG = "yaml/webConfiguration.yaml";
   public static final String DRIVER_URL = "http://127.0.0.1:4723";
   public static final String ALLURE_COMMAND_WIN =
